46dc9c269afbe98b78d8664163f59ca9488f1809
Urmat <okonov.okonov@gmail.com>
2018-11-10 23:34:22 +0600

    Main Project
    Project made in Visual Studio 2017

---
+++ Okonov_Urmat_Bisection/Okonov_Urmat_Bisection/BisectionMethodClass.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using parserDecimal.Parser;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Okonov_Urmat_Bisection
+{
+    public class BisectionMethodClass
+    {
+        double eps = 0;
+        public double timepl = 0;
+        public decimal intervalBegin1 = 0;
+        public decimal intervalEnd1 = 0;
+        public decimal middle = 0;
+        public double precision = 0;
+        public Stopwatch stopWatch = new Stopwatch();
+        public Stopwatch time = new Stopwatch();
+        public int init1 = 0;
+        public TimeSpan tim = new TimeSpan();
+        public string elapsedTime1 = "";
+        Computer computer = new Computer();
+        public decimal rightbound = 0.0M, leftbound = 0.0M;
+        public double epsilon = 0;
+        public string func = "",b1="",b2="",b3="";
+        public uint k_max = 0;
+        public double max_time = 0;
+        public int k_max_begin = 0;
+        public double time_max_begin = 0;
+        Bisection_Method b = new Bisection_Method();
+        public event Action<int> increment_max;
+        public event Action<int> time_max;
+
+        public void start(decimal intervalBegin, decimal intervalEnd, double precision, uint K_maxBox, int init, double max_time, string func, System.Windows.Forms.ProgressBar progressBar)
+        {
+            while (Math.Abs(intervalBegin - intervalEnd) > Convert.ToDecimal(precision) && init < Convert.ToInt32(K_maxBox))
+            {
+
+
+                stopWatch.Start();
+                init++;
+
+                middle = (intervalBegin + intervalEnd) / 2.0M;
+
+
+
+
+                if (Math.Sign(GetFunction(func, intervalBegin)) * Math.Sign(GetFunction(func, middle)) < 0.0M)
+                {
+                    intervalEnd = middle;
+
+                }
+                else
+                {
+                    intervalBegin = middle;
+
+                }
+
+
+                progressBar.Visible = true;
+                progressBar.Maximum = Convert.ToInt32(init + 0.00001);
+                progressBar.Value = init;
+          
+            
+
+                stopWatch.Stop();
+                tim = stopWatch.Elapsed;
+                TimeSpan ts1 = stopWatch.Elapsed;
+                elapsedTime1 = String.Format("{00}",
+                ts1.Milliseconds);
+                eps = Convert.ToDouble(elapsedTime1);
+                if (eps < 0.01)
+                {
+                    timepl = 0.01 - eps;
+                }
+                if (eps >= max_time)
+                {
+                    var mb = MessageBox.Show("K_max " + K_maxBox + "\n" + "Time " + max_time + "\n" + "X*= " + middle + "\n" + "Time is up.It isn't possible to find a solution with the given time.Would you like to continue? ", "Warning", MessageBoxButtons.YesNo);
+                    if (mb == DialogResult.Yes)
+                    {
+                        max_time += max_time;
+                        time_max?.Invoke(Convert.ToInt32(max_time));
+
+                    }
+                    else if (mb == DialogResult.No)
+                    {
+                        b1 = "Solution hasn`t found.Time reached the limit.";
+                        break;
+                    }
+
+                }
+
+
+                if (init >= K_maxBox)
+                {
+                    var mb = MessageBox.Show("K_max " + K_maxBox + "\n" + "Time " + max_time + "\n" + "X*= " + middle + "\n" + "iterations reached the limit.It isn't possible to find a solution with the given k_max.Would you like to continue? ", "Warning", MessageBoxButtons.YesNo);
+                    if (mb == DialogResult.Yes)
+                    {
+                        K_maxBox += K_maxBox;
+                        increment_max?.Invoke(Convert.ToInt32(K_maxBox));
+
+                    }
+                    else if (mb == DialogResult.No)
+                    {
+                        
+                        b1 = "Solution hasn`t found.iterations reached the limit.";
+                        break;
+                    }
+
+                }
+
+            }
+
+            stopWatch.Reset();
+            progressBar.Visible = false;
+            timepl = Convert.ToDouble(elapsedTime1);
+            intervalBegin1 = intervalBegin;
+            intervalEnd1 = intervalEnd;
+            init1 = init;
+            k_max_begin = Convert.ToInt32(K_maxBox);
+            time_max_begin = max_time;
+            b3 = b1;
+        }
+
+
+        private decimal GetFunction(string function, decimal x1)
+        {
+            Computer comp = new Computer();
+            return comp.Compute(function, x1);
+        }
+      
+    }
+}
---
+++ Okonov_Urmat_Bisection/Okonov_Urmat_Bisection/Bisection_Method.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using info.lundin.math;
+using parserDecimal.Parser;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Okonov_Urmat_Bisection
+{
+    public partial class Bisection_Method : Form
+    {
+        decimal intervalBegin = 0;
+        decimal intervalEnd = 0;
+        double precision = 0;
+        Stopwatch stopWatch = new Stopwatch();
+        Stopwatch time = new Stopwatch();
+        int init = 0;
+        Computer computer = new Computer();
+        Change_Letters c = new Change_Letters();
+        string func = "";
+        uint k_max = 0;
+        double max_time = 0;
+
+        ExpressionParser parser = new ExpressionParser();
+
+        public Bisection_Method()
+        {
+            InitializeComponent();
+            SolutionOfTaskBox.Text = "";
+            ValueOfFunctionBox.Text = "";
+            BapsedTimeBox.Text = "";
+            AmountOfIteratinosBox.Text = "";
+            AbsBox.Text = "";
+            label11.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            funcBox.Text = c.Function_Verify(funcBox.Text);
+            if (funcBox.Text!="")
+            {
+                if (funcBox.Text == "" || LeftEndPointBox.Text == "" || RightEndPointBox.Text == "" || ToleranceBox.Text == "" || timeBox.Text == "" || K_maxBox.Text == "")
+                {
+                    MessageBox.Show("Input textboxes are empty! Enter the data");
+                    label11.Text = "Input textboxes are empty! Enter the data";
+                    return;
+                }
+
+                func = funcBox.Text.Trim().Replace(".", ",");
+                func = func.ToLower();
+                label11.Text = "";
+                try
+                {
+                    k_max = UInt16.Parse(K_maxBox.Text.Trim());
+                }
+                catch (Exception h)
+                {
+                    MessageBox.Show("Character values are not allowed.Check k_max.");
+                    label11.Text = "Character values are not allowed.Check k_max.";
+                    return;
+                }
+                if (k_max <= 0)
+                {
+                    MessageBox.Show("k_max can`t be less or equal than zero");
+                    label11.Text = "k_max can`t be less or equal than zero";
+                    return;
+                }
+                try
+                {
+                    intervalBegin = Decimal.Parse(LeftEndPointBox.Text.Trim(), System.Globalization.NumberStyles.Float);
+                }
+                catch (Exception h)
+                {
+                    MessageBox.Show("Character values are not allowed.Check left bound value.");
+                    label11.Text = "Character values are not allowed.Check left bound value.";
+                    return;
+                }
+                try
+                {
+                    intervalEnd = Decimal.Parse(RightEndPointBox.Text.Trim(), System.Globalization.NumberStyles.Float);
+                }
+                catch (Exception h)
+                {
+                    MessageBox.Show("Character values are not allowed.Check right bound value.");
+                    label11.Text = "Character values are not allowed.Check right bound value.";
+                    return;
+                }
+                try
+                {
+                    precision = Double.Parse(ToleranceBox.Text);
+                }
+                catch (Exception h)
+                {
+                    MessageBox.Show("Character values are not allowed.Check the Tolerance textbox.");
+                    label11.Text = "Character values are not allowed.Check the Tolerance textbox.";
+                    return;
+                }
+                try
+                {
+                    max_time = Double.Parse(timeBox.Text.Trim().Replace(".", ","));
+                }
+                catch (Exception h)
+                {
+                    MessageBox.Show("Character values are not allowed.Check the max_time textbox.");
+                    label11.Text = "Character values are not allowed.Check the max_time textbox.";
+                    return;
+                }
+                string str = "-";
+
+                if (str == ToleranceBox.Text[0].ToString() || ToleranceBox.Text.ToString() == "0")
+                {
+                    MessageBox.Show("Tolerance can`t be less than zero or equal it");
+                    label11.Text = "Tolerance can`t be less than zero or equal it ";
+                    return;
+                }
+                if (max_time <= 0)
+                {
+                    MessageBox.Show("Time limit can`t be less or equal than zero");
+                    label11.Text = "Time limit can`t be less or equal than zero";
+                    return;
+                }
+                if (funcBox.Text == "$" || funcBox.Text == "#" || funcBox.Text == "@" || funcBox.Text == "&" || funcBox.Text == "$" || funcBox.Text == "`" || funcBox.Text == "?" || funcBox.Text == ";" || funcBox.Text == ":")
+                {
+                    MessageBox.Show("Function incorrect, it doesn`t match to mathematical representation of the function.You are probably added wrong values or incorrect signs like #,@,$,& and etc.Please check the textbox of functions.");
+                    return;
+                }
+
+                GetFunction(func, Convert.ToDecimal(intervalBegin));
+
+                try
+                {
+                    GetFunction(func, Convert.ToDecimal(intervalEnd));
+                }
+                catch (Exception h)
+                {
+                    MessageBox.Show("Function incorrect.Please check the textbox of functions");
+                    label11.Text = "Function incorrect";
+                    return;
+                }
+
+                progressBar.Value = 0;
+
+
+
+
+                if (Math.Sign(Convert.ToDecimal(GetFunction(func, intervalBegin))) * Math.Sign(Convert.ToDecimal(GetFunction(func, intervalEnd))) > 0.0M)
+                {
+                    MessageBox.Show("Function has same signs at ends of interval.Check endpoints of the interval [a, b]!");
+                    label11.Text = "Function has same signs at ends of interval.Check endpoints of the interval [a, b]!";
+                    return;
+                }
+                else
+                {
+                    BisectionMethodClass b = new BisectionMethodClass();
+                    b.increment_max += UpdateIteration;
+                    b.time_max += UpdateTime;
+                    b.start(intervalBegin, intervalEnd, precision, k_max, init, max_time, func, progressBar);
+
+
+                    stopWatch.Reset();
+
+                    ResultPrint(b.middle, GetFunction(func, b.middle), b.elapsedTime1, b.k_max_begin, b.time_max_begin, b.init1, Math.Abs(b.intervalEnd1 - b.intervalBegin1), b.b3);
+                    init = 0;
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Function incorrect.Please check the textbox of functions");
+                label11.Text = "Function incorrect";
+            }
+        }
+        public void ResultPrint(decimal x1, decimal f1, string relError, int k_max_begin, double time_max_begin, int iter_value, decimal rel, string b1)
+        {
+            BisectionMethodClass b = new BisectionMethodClass();
+            SolutionOfTaskBox.Text = x1.ToString();
+            ValueOfFunctionBox.Text = f1.ToString();
+            AmountOfIteratinosBox.Text = iter_value.ToString();
+            BapsedTimeBox.Text = relError.ToString();
+            AbsBox.Text = rel.ToString("0e0");
+            K_maxBox.Text = k_max_begin.ToString();
+            timeBox.Text = time_max_begin.ToString();
+            label11.Text = b1.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SolutionOfTaskBox.Text = "";
+            ValueOfFunctionBox.Text = "";
+            BapsedTimeBox.Text = "";
+            AmountOfIteratinosBox.Text = "";
+            AbsBox.Text = "";
+            label11.Text = "";
+        }
+
+        private decimal GetFunction(string function, decimal x1)
+        {
+            Computer comp = new Computer();
+            return comp.Compute(function, x1);
+        }
+        private void UpdateIteration(int iteration)
+        {
+            Action action = () =>
+            {
+                K_maxBox.Text = iteration.ToString();
+            };
+            Invoke(action);
+        }
+
+        private void UpdateTime(int iteration)
+        {
+            Action action = () =>
+            {
+                timeBox.Text = iteration.ToString();
+            };
+            Invoke(action);
+        }
+    
+    }
+}
+
